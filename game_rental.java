464eb95db91738fd90c929be449a325ce584a12b
bspiekermann2 <spiekermann.2@wright.edu>
2022-09-24 20:44:58 -0400

    Best working version

--- game_rental.java
+++ game_rental.java
@@ -6,13 +6,14 @@ public class game_rental {
     static int dayCInt, monthCInt, yearCInt, dayDInt, monthDInt, yearDInt;
     static Scanner theScanner = new Scanner(System.in);
     static boolean validDate = false;
-    static String ratings[] = new String [6];
     static String checkParts[], dobParts[] = new String [3];
     static int month[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    static String ratings[] = {"EC", "E", "E10", "T", "M", "AO"};
+    
 
     public static int[] calcAge (int dayCInt, int monthCInt, int yearCInt, int dayDInt, int monthDInt, int yearDInt) {
         if (dayDInt > dayCInt) {
-            dayCInt = dayCInt + month[dayDInt - 1];
+            dayCInt = dayCInt + month[monthDInt - 1];
             monthCInt = monthCInt - 1;
         }
         if (monthDInt > monthCInt) {
@@ -27,7 +28,7 @@ public class game_rental {
     }
 
     public static void main (String args[]){
-        do{
+        do {
             System.out.print("Please enter checkout date (MM/DD/YYYY): ");
             checkDate = theScanner.nextLine();
             System.out.print("Please enter date of birth (MM/DD/YYYY): ");
@@ -38,34 +39,35 @@ public class game_rental {
             // This try catch make sure that only integers are entered into the date requests
             try{
                 String [] checkParts = checkDate.split("/");
-                int dayCInt = Integer.parseInt(checkParts[1]);
-                int monthCInt = Integer.parseInt(checkParts[0]);
-                int yearCInt = Integer.parseInt(checkParts[2]);
+                dayCInt = Integer.parseInt(checkParts[1]);
+                monthCInt = Integer.parseInt(checkParts[0]);
+                yearCInt = Integer.parseInt(checkParts[2]);
 
                 String [] dobParts = dob.split("/");
-                int dayDInt = Integer.parseInt(dobParts[1]);
-                int monthDInt = Integer.parseInt(dobParts[0]);
-                int yearDInt = Integer.parseInt(dobParts[2]);
-                System.out.printf("\nCheckout: %d,%d,%d (day,month,year)\nDOB: %d,%d,%d (day,month,year)\nGame Rating: %s",dayCInt,monthCInt,yearCInt,dayDInt,monthDInt,yearDInt,gameRate);
+                dayDInt = Integer.parseInt(dobParts[1]);
+                monthDInt = Integer.parseInt(dobParts[0]);
+                yearDInt = Integer.parseInt(dobParts[2]);
+                System.out.printf("\nCheckout: %d,%d,%d (day,month,year)\nDOB: %d,%d,%d (day,month,year)\nGame Rating: %s",
+                dayCInt,monthCInt,yearCInt,dayDInt,monthDInt,yearDInt,gameRate);
 
             } catch (NumberFormatException e1){
                 System.out.print("\nPlease enter dates with the proper format (MM/DD/YYYY)");
                 theScanner = new Scanner(System.in);
+
             }
 
             // This section checks the game rating that was entered to be sure it is valid
-            String ratings[] = {"EC", "E", "E10", "T", "M", "AO"};
             if (!gameRate.equals(ratings[0]) && !gameRate.equals(ratings[1]) && !gameRate.equals(ratings[2]) && !gameRate.equals(ratings[3]) && !gameRate.equals(ratings[4]) && !gameRate.equals(ratings[5])) {
                 System.out.print("\nPlease enter valid game rating (EC,E,E10,T,M,AO)\n");
 
-            } else if ( monthDInt == 4 || monthDInt == 6 || monthDInt == 9 || monthDInt == 11 || monthCInt == 4 || monthCInt == 6 || monthCInt == 9 || monthCInt == 11 && dayCInt > 30 || dayDInt > 30) {
+            } else if ((monthDInt == 4 || monthDInt == 6 || monthDInt == 9 || monthDInt == 11 || monthCInt == 4 || monthCInt == 6 || monthCInt == 9 || monthCInt == 11) && (dayCInt > 30 || dayDInt > 30)) {
                 System.out.print("\nPlease enter a valid day for the month entered\n");
 
-            } else if (monthDInt == 1 || monthDInt == 3 || monthDInt == 5 || monthDInt == 7 || monthDInt == 8 || monthDInt == 10 || monthDInt == 12 || monthCInt == 1 || monthCInt == 3 || monthCInt == 5 ||
-            monthCInt == 7 || monthCInt == 8 || monthCInt == 10 || monthCInt == 12 && dayDInt > 31 || dayCInt > 31) {
+            } else if ((monthDInt == 1 || monthDInt == 3 || monthDInt == 5 || monthDInt == 7 || monthDInt == 8 || monthDInt == 10 || monthDInt == 12 || monthCInt == 1 || monthCInt == 3 || monthCInt == 5 ||
+            monthCInt == 7 || monthCInt == 8 || monthCInt == 10 || monthCInt == 12) && (dayDInt > 31 || dayCInt > 31)) {
                 System.out.print("\nPlease enter a valid day for the month entered\n");
 
-            } else if (monthDInt == 2 || monthCInt == 2 && dayDInt > 29 || dayCInt > 29) {
+            } else if ((monthDInt == 2 || monthCInt == 2) && (dayDInt > 29 || dayCInt > 29)) {
                 System.out.print("\nPlease enter a valid day for the month entered\n");
 
             } else if (yearDInt > yearCInt) {
@@ -93,7 +95,7 @@ public class game_rental {
         if (gameRate.equals(ratings[0])) {
             System.out.print("\nRental Allowed");
 
-        } else if (gameRate.equals(ratings[1]) && ageFinal[0] >= 8 && ageFinal[1] >= 6 && ageFinal[2] >= 15){
+        } else if (gameRate.equals(ratings[1]) && ageFinal[0] >= 8 && ageFinal[1] >= 6){
             System.out.print("\nRental Allowed");
 
         } else if (gameRate.equals(ratings[2]) && ageFinal[0] >= 10) {
